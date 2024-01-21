cb1aa87f9cfcd367386d94dce9897953a4a56a4b
ihorkavun030983 <ihorkavun1234@gmail.com>
2024-01-21 23:56:59 +0200

    HW React Table

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+class AnimalsTable extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      animals: [
+        { type: `turtle`, icon: `🐢` },
+        { type: `octopus`, icon: `🐙` },
+        { type: `fish`, icon: `🐠` },
+        { type: `flamingo`, icon: `🦩` },
+        { type: `penguin`, icon: `🐧` },
+      ],
+    };
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      const randomIndex = Math.floor(Math.random() * this.state.animals.length);
+      const newAnimals = [...this.state.animals];
+      newAnimals[randomIndex].color = `green`;
+      newAnimals[randomIndex].fontWeight = `bold`;
+      if (randomIndex === Math.floor(this.state.animals.length / 2)) {
+        this.setState({ border: `10px solid black` });
+      }
+      if (newAnimals.every((animal) => animal.color === `green`)) {
+        this.setState({ border: `20px solid black` });
+      }
+
+      this.setState({ animals: newAnimals });
+    }, 2000);
+  }
+
+
+render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <table style={{ border: this.state.border }}>
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Icon</th>
+        </tr>
+      </thead>
+      <tbody>
+        {this.state.animals.map((animal, index) => (
+          <tr key={index} style={{ color: animal.color, fontWeight: animal.fontWeight }}>
+            <td>{animal.type}</td>
+            <td>{animal.icon}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 }
+}
 
-export default App;
+export default AnimalsTable;
+
+
